42aa27f4f5a917abd6d5748e6a5a85ecd6f9927f
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-06-21 15:43:58 +0200

    extra 5

--- homeworkExtra.js
+++ homeworkExtra.js
@@ -67,3 +67,20 @@ and punctuation in determining if the string is a palindrome.
 const isPolindrome = (str) =>
   str.split(" ").join("") ===
   str.split(" ").join("").split("").reverse().join("");
+
+
+  /* 5) REVERSE INT
+
+Given an integer, return an integer that is the reverse
+ordering of numbers.
+
+--- Examples
+
+    reverseInt(15) === 51
+    reverseInt(981) === 189
+    reverseInt(500) === 5
+    reverseInt(-15) === -51
+    reverseInt(-90) === -9
+ */
+const reverseInt = (n) =>
+parseFloat(n.toString().split("").reverse().join("")) * Math.sign(n);
