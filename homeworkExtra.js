e63225c2763fb6197ae3a55b359fafd88ae2b26c
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-06-21 15:41:56 +0200

    extra 3

--- homeworkExtra.js
+++ homeworkExtra.js
@@ -8,18 +8,18 @@ commonly used in the string.
     maxChar("apple 1231111") === "1"
 */
 const maxChar = (str) => {
-    let max = 0;
-    let maxChar = "";
-    str.split("").forEach((char) => {
-      if (str.split(char).length > max) {
-        max = str.split(char).length;
-        maxChar = char;
-      }
-    });
-    return maxChar;
-  };
+  let max = 0;
+  let maxChar = "";
+  str.split("").forEach((char) => {
+    if (str.split(char).length > max) {
+      max = str.split(char).length;
+      maxChar = char;
+    }
+  });
+  return maxChar;
+};
 
-  /* 2) ANAGRAMS
+/* 2) ANAGRAMS
 
 Check to see if two provided strings are anagrams of each other.
 One string is an anagram of another if it uses the same characters
@@ -32,4 +32,22 @@ or punctuation.  Consider capital letters to be the same as lower case
   anagrams('Hi there', 'Bye there') --> False
 */
 const isAnagram = (str1, str2) =>
-str1.split("").sort().join("") === str2.split("").sort().join("");
+  str1.split("").sort().join("") === str2.split("").sort().join("");
+
+/* 3) ANAGRAMS 2
+
+Given a word and a list of possible anagrams, select the correct sublist.
+
+--- Examples 
+
+    "listen" and a list of candidates like "enlists" "google" "inlets" "banana" the program should return a list containing "inlets".
+*/
+const anagrams2 = (str, arr) => {
+  const anagrams = [];
+  arr.forEach((word) => {
+    if (isAnagram(str, word)) {
+      anagrams.push(word);
+    }
+  });
+  return anagrams;
+};
