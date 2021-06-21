49e16edfccd4806cbf79cfa65904e7609ee4456e
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-06-21 15:38:24 +0200

    extra 2

--- homeworkExtra.js
+++ homeworkExtra.js
@@ -18,3 +18,18 @@ const maxChar = (str) => {
     });
     return maxChar;
   };
+
+  /* 2) ANAGRAMS
+
+Check to see if two provided strings are anagrams of each other.
+One string is an anagram of another if it uses the same characters
+in the same quantity. Only consider characters, not spaces
+or punctuation.  Consider capital letters to be the same as lower case
+
+--- Examples
+  anagrams('rail safety', 'fairy tales') --> True
+  anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+  anagrams('Hi there', 'Bye there') --> False
+*/
+const isAnagram = (str1, str2) =>
+str1.split("").sort().join("") === str2.split("").sort().join("");
