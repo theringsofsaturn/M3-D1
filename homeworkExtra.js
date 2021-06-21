b8b32cd051b10a43fa7c2a39cfa0e6a58d69411e
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-06-21 15:46:58 +0200

    extra 8

--- homeworkExtra.js
+++ homeworkExtra.js
@@ -134,3 +134,29 @@ order of characters
     reverse('Greetings!') === '!sgniteerG'
  */
 const reverseStr = (str) => str.split("").reverse().join("");
+
+
+/* 8) CHUNK
+
+Given an array and chunk size, divide the array into many subarrays
+where each subarray is of length size
+
+--- Examples
+
+    chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+    chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+    chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+    chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+    chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+*/
+const chunk = (arr, n) => {
+    const chunks = [];
+    let i = 0;
+    let length = arr.length;
+  
+    while (i < length) {
+      chunks.push(arr.slice(i, (i += n)));
+    }
+    return chunks;
+  };
+  console.log(chunk([1, 2, 3, 4], 2));
